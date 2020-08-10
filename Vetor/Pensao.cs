e9cc8e1162a9a0bfa165b6d50d2464deab60523a
Bruno Martins <bruno.sousa.martins@gmail.com>
2020-08-10 19:56:06 +0100

    Procjeto inicial de Vetores

---
+++ Vetor/Pensao.cs
@@ -0,0 +1,9 @@
+﻿namespace Vetor
+{
+    class Pensao
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+    }
+}
---
+++ Vetor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pensao[] vect = new Pensao[10];
+
+            Console.WriteLine("how many rooms will be rented?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Student name:");
+                string name = Console.ReadLine();
+                Console.Write("Student email:");
+                string email = Console.ReadLine();
+                Console.Write("Student room:");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Pensao { Name = name, Email = email, Room = room };
+
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null) {
+                    Console.WriteLine(vect[i].Name);
+                    Console.WriteLine(vect[i].Email);
+                    Console.WriteLine(vect[i].Room);
+                }
+            }
+        }
+    }
+}
